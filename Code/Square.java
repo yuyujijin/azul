e07e6b782748b6b67cf7114942bed9db11f9194e
Clisson Adam <adamclisson@gmail.com>
2019-12-25 17:48:33 +0100

    Update Square.java

--- Code/Square.java
+++ Code/Square.java
@@ -1,25 +1,25 @@
-public class Square{
-	
+public class Square {
+
 	private Tile t;
 
-	public Square(){
+	public Square() {
 		t = null;
 	}
 
-	public void add(Tile t){
+	public void add(Tile t) {
 		this.t = t;
 	}
 
-	public void remove(){
+	public void remove() {
 		t = null;
 	}
 
-	public boolean isEmpty(){
-		return(t==null);
+	public boolean isEmpty() {
+		return (t == null);
 	}
-	
+
 	public char getColor() {
 		return t.getColor();
 	}
-	
+
 }
