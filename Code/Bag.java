18860b520dafa3a2751f446ea9f3998a73a30dd4
Clisson Adam <adamclisson@gmail.com>
2019-12-25 17:45:24 +0100

    Update Bag.java

--- Code/Bag.java
+++ Code/Bag.java
@@ -1,41 +1,55 @@
-//We should had import to a xml file, for all package
+
+//We should import a xml file, for all package
+
 import java.util.*;
 
-public class Bag{
-  
-	private ArrayList<Tile> pieces;
+public class Bag {
 
-    public Bag(){
-    	ArrayList<Tile> p = new ArrayList<Tile>(100);
+	protected ArrayList<Tile> pieces;
 
-    	//On ajoute les 20 pièces de chaque couleur
+	public Bag() {
+		ArrayList<Tile> p = new ArrayList<Tile>(100);
 
-    	for(int i = 0 ; i < 100 ; i++){
-    		if(i<20) p.add(new Tile('r')); //Red
-    		if(i>=20 && i<40) p.add(new Tile('b')); //Blue
-    		if(i>=40 && i<60) p.add(new Tile('g')); //Green
-    		if(i>=60 && i<80) p.add(new Tile('w')); //White
-    		if(i>=80) p.add(new Tile('n')); //Noir
-	    }
-	    pieces = p;
-    }
+		// On ajoute les 20 pièces de chaque couleur
 
-	//Methode pour mélanger
-	public void shuffle(){
+		for (int i = 0; i < 100; i++) {
+			if (i < 20)
+				p.add(new Tile('r')); // Red
+			if (i >= 20 && i < 40)
+				p.add(new Tile('b')); // Blue
+			if (i >= 40 && i < 60)
+				p.add(new Tile('g')); // Green
+			if (i >= 60 && i < 80)
+				p.add(new Tile('w')); // White
+			if (i >= 80)
+				p.add(new Tile('n')); // Noir
+		}
+		pieces = p;
+	}
+
+	// Methode pour mélanger
+	public void shuffle() {
 		Collections.shuffle(pieces);
 	}
 
-    //Methode pour voir si vide
-    public boolean isEmpty(){
-    	return pieces.isEmpty();
-    }
+	// Methode pour voir si vide
+	public boolean isEmpty() {
+		return pieces.isEmpty();
+	}
 
-    //Methode pour prendre une pieces
-    public Tile pick(){
-    	if(!pieces.isEmpty()){
-    		shuffle();
-    		return pieces.remove(0);
-    	}
-    	return null;
-    }
+	// Methode pour prendre une pieces
+	public Tile pick() {
+		if (!pieces.isEmpty()) {
+			shuffle();
+			return pieces.remove(0);
+		}
+		return null;
+	}
+
+	public void refile(Tile[] ts) {
+		if (isEmpty()) {
+			for (Tile t : ts)
+				pieces.add(t);
+		}
+	}
 }
