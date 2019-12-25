27c3f3feeeddbf6725f26ad4638f2ba9cd66c6e2
Clisson Adam <adamclisson@gmail.com>
2019-12-25 17:46:32 +0100

    Add new file

---
+++ Code/Discards.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Discards extends Bag {
+
+	public Discards() {
+		pieces = new ArrayList<Tile>();
+	}
+
+	public void add(Tile[] ts) {
+		for (Tile t : ts) {
+			pieces.add(t);
+		}
+	}
+
+	public Tile[] empty() {
+		Tile[] ts = (Tile[]) pieces.toArray();
+		pieces.clear();
+		return ts;
+	}
+
+	public boolean isEmpty() {
+		return pieces.isEmpty();
+	}
+}
