06aed4a34bfb321adef36baa1da77f703c597dcc
Clisson Adam <adamclisson@gmail.com>
2019-12-27 12:38:51 +0100

    Add new file

---
+++ Code/Vue.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class Vue extends JFrame {
+
+	public Vue() {
+		super("Azul");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setPreferredSize(new Dimension(1080, 720));
+
+		setLocation(200, 200);
+		pack();
+		setVisible(true);
+		this.setContentPane(new Panneau());
+
+	}
+}
