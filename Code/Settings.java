88ac94dce8b13ac6ed54c27656c9b995a6d0434a
Clisson Adam <adamclisson@gmail.com>
2020-01-02 21:27:01 +0100

    adding javadoc and updatind code

--- Code/Settings.java
+++ Code/Settings.java
@@ -7,12 +7,14 @@ import javax.swing.*;
 
 public class Settings extends JPanel {
 
+	// declaring the differents variables that will be used in the file
 	private Info i = new Info();
 	private boolean sendData;
 	private JLabel playerLabel, tailleLabel, jokerLabel;
 	private JComboBox player, taille;
 	private JCheckBox joker;
 
+	// constructor for Settings, displaying a message
 	public Settings() {
 		initComponent();
 		JOptionPane.showMessageDialog(
@@ -20,18 +22,23 @@ public class Settings extends JPanel {
 	                JOptionPane.INFORMATION_MESSAGE);
 		}
 	
-
+	// get informations from the selected parameters (JComboBox)
 	public Info showInfo() {
 		boolean b = joker.isSelected();
 		return new Info((String) player.getSelectedItem(),(String) taille.getSelectedItem(),b);
 	}
 
+	/** initialises the diffenrents components for the game parameters
+	* such as the differents panels, the JCheckBox, JComboBox
+	**/
 	private void initComponent() {
 
+		//player pannel 
 		JPanel panPlayer = new JPanel();
 		panPlayer.setBackground(Color.white);
 		panPlayer.setPreferredSize(new Dimension(220, 60));
 		panPlayer.setBorder(BorderFactory.createTitledBorder("Nombre de joueurs"));
+		// creating the scrolling menu to select the nb of players
 		player = new JComboBox();
 		player.addItem("2");
 		player.addItem("3");
@@ -40,9 +47,11 @@ public class Settings extends JPanel {
 		panPlayer.add(playerLabel);
 		panPlayer.add(player);
 
+		// size pannel
 		JPanel panTaille = new JPanel();
 		panTaille.setBackground(Color.white);
 		panTaille.setBorder(BorderFactory.createTitledBorder("Taille :"));
+		// creating the scrolling menu to select the wanted size of the game window
 		taille = new JComboBox();
 		taille.addItem("1280x720");
 		taille.addItem("1920x1080");
@@ -51,9 +60,11 @@ public class Settings extends JPanel {
 		panTaille.add(tailleLabel);
 		panTaille.add(taille);
 		
+		// pannel for the joker option
 		JPanel panJoker = new JPanel();
 		panTaille.setBackground(Color.white);
 		panTaille.setBorder(BorderFactory.createTitledBorder("Joker :"));
+		// creating the joker checkbox
 		joker = new JCheckBox();
 		jokerLabel = new JLabel("Joker :");
 		panJoker.add(jokerLabel);
@@ -62,6 +73,7 @@ public class Settings extends JPanel {
 		JPanel content = new JPanel();
 		content.setBackground(Color.white);
 
+		// adding it all to the JFrame
 		content.add(panTaille);
 		content.add(panPlayer);
 		content.add(panJoker);
