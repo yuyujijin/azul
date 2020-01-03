b001c3ecf47ef28af2eab95ced1d3d46f7d32275
Clisson Adam <adamclisson@gmail.com>
2020-01-03 13:30:48 +0100

    adding javadoc to the file

--- Code/Discards.java
+++ Code/Discards.java
@@ -2,30 +2,36 @@ import java.util.*;
 
 public class Discards extends Bag{
   
+  // constructor for the Discard
   public Discards(){
     pieces = new ArrayList<Tile>();
   }
 
+  // add a list tile to the discard
   public void add(Tile[] ts){
     for(Tile t : ts){
       pieces.add(t);
     }
   }
   
+  // add a tile to the discard
   public void add(Tile t) {
 	  pieces.add(t);
   }
 
+  // empty
   public Tile[] empty(){
     Tile[] ts = listToArray(pieces);
     pieces.clear();
     return ts;
   }
 
+  // checks if the list is empty
   public boolean isEmpty(){
     return pieces.isEmpty();
   }
   
+  // changed the list to an array
   public Tile[] listToArray(ArrayList<Tile> al) {
 	  Tile[] treturn = new Tile[al.size()];
       int i = 0;
